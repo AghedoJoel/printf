0eae2604cd310e751907c2009476c5781b89d623
Aghedo.joel <aghedo.joel15@gmail.com>
2023-01-23 01:39:17 -0800

    task _printf.c

--- _printf.c
+++ _printf.c
@@ -30,13 +30,12 @@ free_buffer(output);
 * Return: The number of characters stored to output.
 */
 int run_printf(const char *format, va_list args, buffer_t *output)
-
 {
 int i, wid, prec, ret = 0;
 char tmp;
 unsigned char flags, len;
-unsigned int (*f)(va_list, buffer_t *,
-unsigned char, int, int, unsigned char);
+unsigned int (*f)(va_list, buffer_t *);
+unsigned char, int, int, unsigned char;
 
 for (i = 0; *(format + i); i++)
 {
